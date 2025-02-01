cbaf22bf786f9ddc0f4c3e57df806730e2aa2076
asif1205 <mdyasinraza6@gmail.com>
2025-02-01 14:37:40 +0530

    added one line more

--- add.java
+++ add.java
@@ -2,5 +2,7 @@ public class Add{
     public static void main(String args[]){
         System.out.println("This is learning purpose");
         System.out.println("This program is for testing pruspose");
+        System.out.println("This program is for testing pruspose2");
+
     }
 }
